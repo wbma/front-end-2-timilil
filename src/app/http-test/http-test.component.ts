7cc59bd698ee098ecc5dcafc045553aae69a24a3
timilil <timi.liljestrom@hotmail.com>
2018-01-19 13:22:55 +0200

    message

--- src/app/http-test/http-test.component.ts
+++ src/app/http-test/http-test.component.ts
@@ -9,23 +9,30 @@ import {HttpClient} from '@angular/common/http';
 export class HttpTestComponent implements OnInit {
 
   someData: String = 'hello';
-  imgFolder: String = 'http://media.mw.metropolia.fi/wbma/uploads/'
-  imgURL: String = ''
+  imgFolder: String = 'http://media.mw.metropolia.fi/wbma/uploads/';
+  imgURL: String = '';
+  mediaArray: any;
 
   constructor(private http: HttpClient) { }
 
   /*makes a request and subscribes to the response. Save response into a variable (data) */
   getJSON() {
-    this.http.get('assets/package.json').subscribe((data) => {
+    interface MyInterFace {
+      license: string;
+    }
+
+    this.http.get<MyInterFace>('assets/package.json').subscribe((data) => {
       console.log(data);
-    this.someData = data['license']; //this.someData = data.license;
+      //this.someData = data['license'];
+      this.someData = data.license;
     });
   }
 
   getMedia() {
     this.http.get('http://media.mw.metropolia.fi/wbma/media').subscribe((data) => {
       console.log(data);
-      this.imgURL = data[0].filename;
+      //this.imgURL = data[0].filename;
+      this.mediaArray = data;
     });
   }
 
